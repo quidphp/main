eb5c45051532f7ce38f632ee7737c3dbfaf0f252
QuidPHP <info@quidphp.com>
2019-10-17 18:27:06 +0000

    Apply fixes from StyleCI

--- src/_permission.php
+++ src/_permission.php
@@ -4,7 +4,7 @@ declare(strict_types=1);
 /*
  * This file is part of the QuidPHP package.
  * Website: https://quidphp.com
- * License: https://github.com/quidphp/core/blob/master/LICENSE
+ * License: https://github.com/quidphp/main/blob/master/LICENSE
  */
 
 namespace Quid\Main;
@@ -18,32 +18,32 @@ trait _permission
     // retourne le tableau de la source des paramètres de permission
     // doit retourner une référence
     abstract public function &permissionAll():array;
-    
-    
+
+
     // permissionDefaultRole
     // retourne le role par défaut à utiliser
     abstract public function permissionDefaultRole():Role;
-    
-    
+
+
     // onPermissionCan
     // permet de spécifier une méthode lors de chaque appel à permissionCan
-    protected function onPermissionCan($key,array $array):bool 
+    protected function onPermissionCan($key,array $array):bool
     {
         return true;
     }
-    
-    
+
+
     // permissionRole
     // retourne le tableau de la source des paramètres d'un rôle
     public function permissionRole($role):array
     {
-        $return = array();
+        $return = [];
         $all = $this->permissionAll();
         $key = $role;
-        
+
         if((is_string($role) && is_a($role,Role::class,true)) || $role instanceof Role)
         $key = $role::name();
-        
+
         if(is_string($key))
         {
             if(array_key_exists('*',$all) && is_array($all['*']))
@@ -52,73 +52,73 @@ trait _permission
             if(array_key_exists($key,$all) && is_array($all[$key]))
             $return = Base\Arrs::replace($return,$all[$key]);
         }
-        
+
         else
         static::throw('invalidRole',$role);
-        
+
         return $return;
     }
-    
-    
+
+
     // permissionChange
     // permet de changer le contenu d'une permission pour un rôle
-    public function permissionChange(array $value,$role=null):void 
+    public function permissionChange(array $value,$role=null):void
     {
         $all =& $this->permissionAll();
         $key = '*';
-        
+
         if((is_string($role) && is_a($role,Role::class,true)) || $role instanceof Role)
         $key = $role::name();
-        
+
         elseif(is_string($role))
         $key = $role;
-        
+
         if(empty($all[$key]))
-        $all[$key] = array();
-        
+        $all[$key] = [];
+
         $all[$key] = Base\Arrs::replace($all[$key],$value);
-         
+
         return;
     }
-    
-    
+
+
     // permissionCan
     // retourne vrai si le rôle peut faire l'action
-    public function permissionCan($key,$role,bool $exception=true):bool 
+    public function permissionCan($key,$role,bool $exception=true):bool
     {
         $return = null;
         $one = $this->permissionRole($role);
-        
+
         if($this->onPermissionCan($key,$one))
         {
             if(array_key_exists($key,$one))
             $return = $one[$key];
         }
-        
+
         if(!is_bool($return))
         {
             if($exception === true)
             static::throw($key,$role);
-            
+
             else
             $return = false;
         }
-        
+
         return $return;
     }
-    
-    
+
+
     // hasPermission
     // retourne vrai si toutes les permissions sont accordés
     public function hasPermission(...$keys):bool
     {
         $return = false;
         $role = $this->permissionDefaultRole();
-        
-        foreach ($keys as $key) 
+
+        foreach ($keys as $key)
         {
             $return = $this->permissionCan($key,$role);
-            
+
             if($return !== true)
             break;
         }
