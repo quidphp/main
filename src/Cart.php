535d8414830a90f6cb99c6cba0b21a3cf0eecc15
Pierre-Philippe Emond <emondpph@gmail.com>
2020-02-25 17:38:38 +0000

    cart update

--- src/Cart.php
+++ src/Cart.php
@@ -55,11 +55,80 @@ class Cart extends Map
     }
 
 
-    // getItem
+    // hasItem
+    // retourne vrai si le cart contient l'item au moins une fois
+    final public function hasItem($value):bool
+    {
+        return $this->getIndexFromItem($value) !== null;
+    }
+
+
+    // getIndexFromItem
+    // retourne le premier index du product dans le cart
+    final public function getIndexFromItem($value,bool $strict=false,bool $allowEmpty=false):?int
+    {
+        $return = null;
+        $indexes = $this->getIndexesFromItem($value);
+        $count = count($indexes);
+
+        if($strict === true)
+        {
+            if($count > 1 || ($allowEmpty === false && $count === 0))
+            static::throw('invalidCountForItem',$value,$count);
+        }
+
+        if(!empty($count))
+        $return = current($indexes);
+
+        return $return;
+    }
+
+
+    // getIndexesFromItem
+    // retourne tous les index du product dans le cart
+    final public function getIndexesFromItem($value):array
+    {
+        $return = [];
+        $value = $this->onPrepareValue($value);
+
+        foreach ($this->arr() as $index => $array)
+        {
+            if($array['item'] === $value)
+            $return[] = $index;
+        }
+
+        return $return;
+    }
+
+
+    // getItemFromIndex
     // permet de retourner un item via un index
-    final public function getItem(int $index)
+    final public function getItemFromIndex(int $index)
     {
         return $this->get($index)['item'] ?? null;
+    }
+
+
+    // getQuantityFromIndex
+    // permet de retourner une quantité via un index
+    final public function getQuantityFromIndex(int $index):?int
+    {
+        return $this->get($index)['quantity'] ?? null;
+    }
+
+
+    // getQuantityFromItem
+    // permet de retourner une quantité via un item
+    // envoie une exception si plus d'un même item dans le cart
+    final public function getQuantityFromItem($value):?int
+    {
+        $return = null;
+        $index = $this->getIndexFromItem($value,true,true);
+
+        if($index !== null)
+        $return = $this->getQuantityFromIndex($index);
+
+        return $return;
     }
 
 
@@ -72,9 +141,44 @@ class Cart extends Map
 
 
     // update
+    // permet de mettre à jour la quantité ou les attributs à partir d'un item
+    // attention les attributs sont remplacés
+    final public function update($value,int $quantity,?array $attr=null)
+    {
+        return $this->updateIndex($this->getIndexFromItem($value,true,false),$quantity,$attr);
+    }
+
+
+    // updateQuantity
+    // permet de mettre à jour la quantité à partir d'un item
+    // les attributs ne sont pas remplacés
+    final public function updateQuantity($value,int $quantity)
+    {
+        return $this->updateIndexQuantity($this->getIndexFromItem($value,true,false),$quantity);
+    }
+
+
+    // updateRelative
+    // permet de mettre à jour la quantité ou les attributs de façon relative à partir d'un item
+    // c'est à dire que la quantité est additionner ou supprimer, les attributs sont merges
+    final public function updateRelative($value,?int $quantity=null,?array $attr=null):self
+    {
+        return $this->updateIndexRelative($this->getIndexFromItem($value,true,false),$quantity,$attr);
+    }
+
+
+    // addOrUpdate
+    // ajoute l'item si non existant dans le cart, sinon envoie à update
+    final public function addOrUpdate($value,int $quantity=1,?array $attr=null):self
+    {
+        return ($this->hasItem($value))? $this->update($value,$quantity,$attr):$this->add($value,$quantity,$attr);
+    }
+
+
+    // updateIndex
     // permet de mettre à jour la quantité ou les attributs à partir d'un index
     // attention les attributs sont remplacés
-    final public function update(int $index,int $quantity,?array $attr=null):self
+    final public function updateIndex(int $index,int $quantity,?array $attr=null):self
     {
         $this->checkExists($index);
         $data = $this->arr();
@@ -84,10 +188,10 @@ class Cart extends Map
     }
 
 
-    // updateQuantity
+    // updateIndexQuantity
     // permet de mettre à jour la quantité à partir d'un index
     // les attributs ne sont pas remplacés
-    final public function updateQuantity(int $index,int $quantity):self
+    final public function updateIndexQuantity(int $index,int $quantity):self
     {
         $this->checkExists($index);
         $data = $this->arr();
@@ -97,10 +201,10 @@ class Cart extends Map
     }
 
 
-    // updateRelative
+    // updateIndexRelative
     // permet de mettre à jour la quantité ou les attributs de façon relative à partir d'un index
     // c'est à dire que la quantité est additionner ou supprimer, les attributs sont merges
-    final public function updateRelative(int $index,?int $quantity=null,?array $attr=null):self
+    final public function updateIndexRelative(int $index,?int $quantity=null,?array $attr=null):self
     {
         $this->checkExists($index);
         $data = $this->arr();
@@ -141,23 +245,6 @@ class Cart extends Map
         }
 
         return $this;
-    }
-
-
-    // indexesFromItem
-    // retourne tous les indexes qui contienennt l'item donné en argument
-    final public function indexesFromItem($value):array
-    {
-        $return = [];
-        $value = $this->onPrepareValue($value);
-
-        foreach ($this as $key => $array)
-        {
-            if($array['item'] === $value)
-            $return[] = $key;
-        }
-
-        return $return;
     }
 }
 ?>
--- test/Cart.php
+++ test/Cart.php
@@ -22,10 +22,32 @@ class Cart extends Base\Test
     {
         // construct
         $cart = new Main\Cart([['test',2]]);
+        $cart2 = new Main\Cart([['test',2],['test2',3]]);
 
         // onPrepareValue
 
-        // getItem
+        // hasItem
+        assert($cart->hasItem('test'));
+        assert(!$cart->hasItem('test2'));
+
+        // getIndexFromItem
+        assert($cart->getIndexFromItem('test') === 0);
+        assert($cart->getIndexFromItem('test2') === null);
+
+        // getIndexesFromItem
+        assert($cart->getIndexesFromItem('testz') === []);
+
+        // getItemFromIndex
+        assert($cart->getItemFromIndex(0) === 'test');
+        assert($cart->getItemFromIndex(1) === null);
+
+        // getQuantityFromIndex
+        assert($cart->getQuantityFromIndex(0) === 2);
+        assert($cart->getQuantityFromIndex(1) === null);
+
+        // getQuantityFromItem
+        assert($cart->getQuantityFromItem('test') === 2);
+        assert($cart->getQuantityFromItem('test2') === null);
 
         // add
         $cart->add('test',1,['special'=>true]);
@@ -33,25 +55,38 @@ class Cart extends Base\Test
         assert($cart->isCount(3));
 
         // update
-        $cart->update(0,3);
-        assert($cart->get(0)['quantity'] === 3);
+        $cart2->update('test2',4);
+        assert($cart2->get(1)['quantity'] === 4);
 
         // updateQuantity
+        $cart2->updateQuantity('test2',5);
+        assert($cart2->get(1)['quantity'] === 5);
 
         // updateRelative
-        $cart->updateRelative(0,-2);
+        $cart2->updateRelative('test2',-1);
+        assert($cart2->get(1)['quantity'] === 4);
+
+        // addOrUpdate
+
+        // updateIndex
+        $cart->updateIndex(0,3);
+        assert($cart->get(0)['quantity'] === 3);
+
+        // updateIndexQuantity
+        $cart->updateIndexQuantity(0,3);
+        assert($cart->get(0)['quantity'] === 3);
+
+        // updateIndexRelative
+        $cart->updateIndexRelative(0,-2);
         assert($cart[0]['quantity'] === 1);
-        $cart->updateRelative(0,-2);
+        $cart->updateIndexRelative(0,-2);
         assert($cart->isCount(2));
 
         // commit
 
-        // indexesFromItem
-        $cart->add('test',1,['special'=>true]);
-        assert($cart->indexesFromItem('test') === [1,3]);
-        assert($cart->indexesFromItem('testz') === []);
-
         // map
+        $cart->add('test',1,['special'=>true]);
+        assert($cart->getIndexesFromItem('test') === [1,3]);
         $cart->filter(function(array $array) {
             ['item'=>$item] = $array;
             return $item === 'test2';
